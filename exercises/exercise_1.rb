74b2904fecc50bfe4ded34d2dc1ad1102ce2786e
dylanToews <dylan.toews@yahoo.ca>
2022-12-12 19:57:07 -0700

    add three stores and revenure to db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,16 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+class Stores < ActiveRecord::Base
 # Your code goes below here ...
+
+Stores.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: true, womens_apparel: true )
+
+Stores.create(name: "Richmond", annual_revenue: "1260000", mens_apparel: false, womens_apparel: true )
+
+Stores.create(name: "Gastown", annual_revenue: "190000", mens_apparel: true, womens_apparel: false )
+
+puts Stores.count
+end
+
+
